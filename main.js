4afe8ff465e01b329f2e93c5adc164ceb714e080
Sequoia McDowell <sequoia.mcdowell@gmail.com>
2012-07-06 19:21:39 -0400

    attempting to require() mousetrap

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 var lsss; //so I can access it from the console
-require(['jquery','underscore','lss'],function($,_,lss){
+require.config({
+	paths : {
+		'Mousetrap' : 'jam/mousetrap/mousetrap'
+	},
+	shim : {
+		'Mousetrap' : {
+			exports : function(){
+				return this.Mousetrap;
+			}
+		}
+	}
+});
+
+require(['jquery','underscore','Mousetrap','lss'],function($,_,Mousetrap,lss){
 	$(function(){
+		console.log(Mousetrap);
+		console.log(this.Mousetrap);
 		lsss=lss;
 		lss.init();
 	});
